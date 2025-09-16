44799e66fdfede8efefd9e7e9744d03938adb8d5
Benzel <benawhiting@gmail.com>
2025-09-16 15:59:42 -0700

    Add User and Post associations and validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true, length: { in: 5..100 }
+  validates :body, presence: true, length: { minimum: 10 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :posts
+  has_secure_password
+
+  validates :username, presence: true, uniqueness: true, length: { in: 3..20 }
+  validates :password, presence: true, length: { minimum: 6 }
 end
