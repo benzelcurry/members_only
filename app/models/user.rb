cf3aeda81228b7d4b1fb6eed16dc590e2856dd4e
Benzel <benawhiting@gmail.com>
2025-09-16 16:11:54 -0700

    Add Devise and modify User model accordingly

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :posts
-  has_secure_password
 
   validates :username, presence: true, uniqueness: true, length: { in: 3..20 }
-  validates :password, presence: true, length: { minimum: 6 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_16_223625) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_16_230544) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -25,6 +25,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_16_223625) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "posts", "users"
